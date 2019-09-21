d96db34532fa6110ced31af1730fc18770bf91c3
Karol Wiśniewski <karol.wisniewski@ecom.software>
2019-09-21 13:28:24 +0200

    Init spring boot app

---
+++ src/main/java/pl/progser/ProgserApiApplication.java
@@ -0,0 +1,13 @@
+package pl.progser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgserApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgserApiApplication.class, args);
+    }
+
+}
